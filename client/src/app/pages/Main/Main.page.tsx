cc201dbe6eb3ad1c4d5a1c5d83da2358bd7f9991
Anton Kiosia <kesya273@gmail.com>
2025-07-06 17:24:56 +0500

    client / adaprive verions for mixins and main page

--- client/src/app/pages/Main/Main.page.tsx
+++ client/src/app/pages/Main/Main.page.tsx
@@ -29,7 +29,7 @@ const MainPage: FC = () => {
             </section>
             <section className={styles.address}>
                 <div className={styles.addressHeading + ' ' + themeTernary}>Наш офис и место получения заказов</div>
-                <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A9cd8f5a4976c1bb039e234bf5e46cdb3cf15f248e995c02b6b8bdbddd1a06216&amp;source=constructor" width="100%" height="500"></iframe>
+                <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A9cd8f5a4976c1bb039e234bf5e46cdb3cf15f248e995c02b6b8bdbddd1a06216&amp;source=constructor" width="100%"></iframe>
             </section>
         </>
     )
